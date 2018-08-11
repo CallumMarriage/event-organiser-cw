f3764b3dd6e3e3a0224fcaac8f9b6152f3a08266
Callum Marriage <callum.marriage@capgemini.com>
2018-08-11 12:42:13 +0100

    differentiate between different calls

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from "react-redux"
-import store from "./store"
 
 ReactDOM.render(
-        <Provider store={store}>
-            <App /> 
-        </Provider>,
+            <App /> ,
     document.getElementById('root')
 );
 
